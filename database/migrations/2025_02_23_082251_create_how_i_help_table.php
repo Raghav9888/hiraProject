2f3b99a4e27c747ed10ac41ad6c0be3a19bd9a1b
Amit <amit0511989@gmail.com>
2025-02-23 17:28:35 +0530

    add new term button

--- database/migrations/2025_02_23_082251_create_how_i_help_table.php
+++ database/migrations/2025_02_23_082251_create_how_i_help_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('how_i_help', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
             $table->timestamps();
         });
     }
