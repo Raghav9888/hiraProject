ccc32a72ae0effa4a668621afaed225b2ef37e5b
Raghav joshi <113655951+Raghav9888@users.noreply.github.com>
2025-03-03 04:26:36 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
                     </div>
                     <div class="dropdown">
                         <select class="form-select" id="practitionerType" aria-label="Default select example"
-                                style="border-radius: 30px !important;padding: 12px 15px 12px 40px;text-align: start;">
+                                style="border-radius: 30px !important;padding:11px 37px 12px 20px;text-align: start;">
                             <option value="">Select type</option>
                             <option value="in-person">In person Offering</option>
                             <option value="virtual">Virtual Practitioners Only</option>
