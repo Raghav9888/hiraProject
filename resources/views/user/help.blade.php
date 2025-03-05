c75b59f4c3ba2dc367b19d0696afa37a0065da19
Raghav joshi <113655951+Raghav9888@users.noreply.github.com>
2025-03-05 18:18:01 +0530

    Update help.blade.php

--- resources/views/user/help.blade.php
+++ resources/views/user/help.blade.php
@@ -6,11 +6,16 @@
             <div class="row">
                 @include('layouts.partitioner_nav')
                 <div class="appointment-wrrpr justify-content-start align-items-start">
-                    <p class="text-start mt-2">
-                        For Tech Help: Please reach out to our technical support specialist: <a href="mailto:technicalsupport@thehiracollective.com">technicalsupport@thehiracollective.com</a>
+                    <p class="text-center mt-2">
+                        <span
+                            class="fw-bold">For Tech Help: Please reach out to our technical support specialist:</span>
+                        <a href="mailto:technicalsupport@thehiracollective.com">technicalsupport@thehiracollective.com</a>
                     </p>
-                    <p class="text-start mt-2">
-                        For Business help: Please reach out here: <a href="mailto:community@thehiracollective.com">community@thehiracollective.com</a>
+                    <p class="text-center mt-2">
+                        <span class="fw-bold">
+                        For Business help: Please reach out here:
+                        </span>
+                        <a href="mailto:community@thehiracollective.com">community@thehiracollective.com</a>
                     </p>
                 </div>
             </div>
