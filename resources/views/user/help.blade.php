df0c9f756773eb777948463e5a91e3c1945b711a
Raghav joshi <113655951+Raghav9888@users.noreply.github.com>
2025-03-05 18:18:45 +0530

    Update help.blade.php

--- resources/views/user/help.blade.php
+++ resources/views/user/help.blade.php
@@ -5,19 +5,19 @@
             @include('layouts.partitioner_sidebar')
             <div class="row">
                 @include('layouts.partitioner_nav')
-                <div class="appointment-wrrpr justify-content-start align-items-start">
-                    <p class="text-center mt-2">
+            </div>
+            <div class="appointment-wrrpr justify-content-start align-items-start mt-5">
+                <p class="text-center mt-2">
                         <span
                             class="fw-bold">For Tech Help: Please reach out to our technical support specialist:</span>
-                        <a href="mailto:technicalsupport@thehiracollective.com">technicalsupport@thehiracollective.com</a>
-                    </p>
-                    <p class="text-center mt-2">
+                    <a href="mailto:technicalsupport@thehiracollective.com">technicalsupport@thehiracollective.com</a>
+                </p>
+                <p class="text-center mt-2">
                         <span class="fw-bold">
                         For Business help: Please reach out here:
                         </span>
-                        <a href="mailto:community@thehiracollective.com">community@thehiracollective.com</a>
-                    </p>
-                </div>
+                    <a href="mailto:community@thehiracollective.com">community@thehiracollective.com</a>
+                </p>
             </div>
         </div>
     </section>
