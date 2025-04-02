febfe9c607a1418858caab6a9d09b3cc1bab6c72
Amritpal Singh <amritsingh83282@gmail.com>
2025-04-02 22:01:27 +0530

    add the email on booking confirm

---
+++ app/Mail/BookingConfirmationMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class BookingConfirmationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $practitionerEmailTemplate;
+    public $intakeForms;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($user, $practitionerEmailTemplate, $intakeForms)
+    {
+        $this->user = $user;
+        $this->practitionerEmailTemplate = $practitionerEmailTemplate;
+        $this->intakeForms = $intakeForms;
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->subject("{$this->user->first_name} {$this->user->last_name}, Your Booking on The Hira Collective is Confirmed 🌸")
+                    ->view('emails.booking_confirmation')
+                    ->with([
+                        'user' => $this->user,
+                        'practitionerEmailTemplate' => $this->practitionerEmailTemplate,
+                        'intakeForms' => $this->intakeForms,
+                    ]);
+    }
+}
