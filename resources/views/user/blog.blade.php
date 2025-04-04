38ef3e130c097debb3170ec1d4e3da934842f856
Raghav joshi <113655951+Raghav9888@users.noreply.github.com>
2025-04-04 17:26:43 +0530

    Update blog.blade.php

--- resources/views/user/blog.blade.php
+++ resources/views/user/blog.blade.php
@@ -13,55 +13,55 @@
                 <button class="blog-search-btn">Search</button>
             </div>
         </div>
-        <div class="recent-resources">
-            <div class="container">
-                <h1 class="home-title mb-3">Recent Resources</h1>
-                <div class="row">
-                    @if(!$blogs->isEmpty())
-                        @foreach($blogs as $blog)
+{{--        <div class="recent-resources">--}}
+{{--            <div class="container">--}}
+{{--                <h1 class="home-title mb-3">Recent Resources</h1>--}}
+{{--                <div class="row">--}}
+{{--                    @if(!$blogs->isEmpty())--}}
+{{--                        @foreach($blogs as $blog)--}}
 
-                            <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
-                                <a href="{{route('blogDetail', $blog->slug)}}" style="text-decoration: none;"
-                                   class="resources-body">
-                                    <div class="row g-0">
-                                        <div class="col-md-4 px-2">
-                                            @php
-                                                $mediaPath = config('app.media_path', 'uploads');
-                                                $localPath = config('app.local_path', 'assets');
+{{--                            <div class="col-sm-12 col-md-6 col-lg-6 mb-4">--}}
+{{--                                <a href="{{route('blogDetail', $blog->slug)}}" style="text-decoration: none;"--}}
+{{--                                   class="resources-body">--}}
+{{--                                    <div class="row g-0">--}}
+{{--                                        <div class="col-md-4 px-2">--}}
+{{--                                            @php--}}
+{{--                                                $mediaPath = config('app.media_path', 'uploads');--}}
+{{--                                                $localPath = config('app.local_path', 'assets');--}}
 
-                                                $imageUrl = $blog->image
-                                                    ? asset("$mediaPath/admin/blog/{$blog->image}")
-                                                    : asset("$localPath/images/no_image.png");
+{{--                                                $imageUrl = $blog->image--}}
+{{--                                                    ? asset("$mediaPath/admin/blog/{$blog->image}")--}}
+{{--                                                    : asset("$localPath/images/no_image.png");--}}
 
-                                            @endphp
+{{--                                            @endphp--}}
 
 
 
-                                            <img src="{{$imageUrl}}" alt="calm" height="160" width="160" class="rounded-4">
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="card-body">
-                                                <h5>{{$blog->name}}</h5>
-                                                <button>{{@$blog->category->name}}</button>
-                                                <p>{{date('M d, Y', strtotime($blog->date))}}</p>
-                                            </div>
-                                        </div>
-                                    </div>
+{{--                                            <img src="{{$imageUrl}}" alt="calm" height="160" width="160" class="rounded-4">--}}
+{{--                                        </div>--}}
+{{--                                        <div class="col-md-8">--}}
+{{--                                            <div class="card-body">--}}
+{{--                                                <h5>{{$blog->name}}</h5>--}}
+{{--                                                <button>{{@$blog->category->name}}</button>--}}
+{{--                                                <p>{{date('M d, Y', strtotime($blog->date))}}</p>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
 
-                                </a>
-                            </div>
-                        @endforeach
-                    @else
-                        <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
-                            <p class="text-center">No Blogs found..</p>
-                        </div>
-                    @endif
-                    {{-- <div class="d-flex justify-content-center">
-                        <button class="home-blog-btn">Load More</button>
-                    </div> --}}
-                </div>
-            </div>
-        </div>
+{{--                                </a>--}}
+{{--                            </div>--}}
+{{--                        @endforeach--}}
+{{--                    @else--}}
+{{--                        <div class="col-sm-12 col-md-6 col-lg-6 mb-4">--}}
+{{--                            <p class="text-center">No Blogs found..</p>--}}
+{{--                        </div>--}}
+{{--                    @endif--}}
+{{--                    --}}{{-- <div class="d-flex justify-content-center">--}}
+{{--                        <button class="home-blog-btn">Load More</button>--}}
+{{--                    </div> --}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
     <div class="blog-category-wrrpr blog-section">
         <div class="container">
@@ -76,49 +76,53 @@
                 <div class="item">Energy Healing</div>
             </div>
             <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
-                    <div class="resources-body">
-                        <img src="{{url('assets/images/calm.png')}}" alt="calm">
-                        <div>
-                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>
-                            <button>Spirituality</button>
-                            <p>February 3, 2025</p>
-                        </div>
-                    </div>
+                <div class="col-md-12 text-center">
+                    <h3>Coming soon...</h3>
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
-                    <div class="resources-body">
-                        <img src="{{url('assets/images/calm.png')}}" alt="calm">
-                        <div>
-                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>
-                            <button>Spirituality</button>
-                            <p>February 3, 2025</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
-                    <div class="resources-body">
-                        <img src="{{url('assets/images/calm.png')}}" alt="calm">
-                        <div>
-                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>
-                            <button>Spirituality</button>
-                            <p>February 3, 2025</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">
-                    <div class="resources-body">
-                        <img src="{{url('assets/images/calm.png')}}" alt="calm">
-                        <div>
-                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>
-                            <button>Spirituality</button>
-                            <p>February 3, 2025</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center">
-                    <button class="home-blog-btn">Load More</button>
-                </div>
+
+{{--                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">--}}
+{{--                    <div class="resources-body">--}}
+{{--                        <img src="{{url('assets/images/calm.png')}}" alt="calm">--}}
+{{--                        <div>--}}
+{{--                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>--}}
+{{--                            <button>Spirituality</button>--}}
+{{--                            <p>February 3, 2025</p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">--}}
+{{--                    <div class="resources-body">--}}
+{{--                        <img src="{{url('assets/images/calm.png')}}" alt="calm">--}}
+{{--                        <div>--}}
+{{--                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>--}}
+{{--                            <button>Spirituality</button>--}}
+{{--                            <p>February 3, 2025</p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">--}}
+{{--                    <div class="resources-body">--}}
+{{--                        <img src="{{url('assets/images/calm.png')}}" alt="calm">--}}
+{{--                        <div>--}}
+{{--                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>--}}
+{{--                            <button>Spirituality</button>--}}
+{{--                            <p>February 3, 2025</p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-sm-12 col-md-6 col-lg-6 mb-4">--}}
+{{--                    <div class="resources-body">--}}
+{{--                        <img src="{{url('assets/images/calm.png')}}" alt="calm">--}}
+{{--                        <div>--}}
+{{--                            <h5>Finding Calm in Chaos: The Ancient Wisdom of Mindfulness Practices for Anxiety</h5>--}}
+{{--                            <button>Spirituality</button>--}}
+{{--                            <p>February 3, 2025</p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="d-flex justify-content-center">--}}
+{{--                    <button class="home-blog-btn">Load More</button>--}}
+{{--                </div>--}}
             </div>
         </div>
     </div>
