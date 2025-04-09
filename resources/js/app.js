c54c91239fb8cfc193c90e495ee1df16021a0893
Raghav joshi <113655951+Raghav9888@users.noreply.github.com>
2025-04-09 06:35:07 +0530

    Fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import './loader.js';
+
+console.log('hello vite')
+window.loadingScreen.addPageLoading()
+import './script.js';
+import './currency_helper.js';
+import './calendar.js';
+import './booking_calendar.js';
+import './search.js';
+
+console.log('remove loafer')
+
+window.loadingScreen.removeLoading();
+
+
+
