5bffa91ac49ac0322a1cd4707de42fddc8578682
Raghav joshi <113655951+Raghav9888@users.noreply.github.com>
2025-05-12 06:33:11 +0530

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -19,6 +19,7 @@
                         </ul>
                     </div>
                     <form class="pt-4" id="subscribe">
+                        @csrf
                         <div class="form-group row align-items-center">
                             <div class="col-md-9">
                                 <input type="email" name="email" class="form-control rounded-4" placeholder="Enter your email" id="email" required>
